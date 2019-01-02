d3a850c47cf54687892c59b67d1b587b5f2be453
Dan DeFelippi <dan@driverdan.com>
2019-01-02 21:40:17 +0000

    initial commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "app_name" {
+  description = "Unique name of the app"
+  type = "string"
+  default = "dancrowdboticscom-dan-55"
+}
+
+variable "custom_domain" {
+  description = "Custom domain name (optional)"
+  type = "string"
+  default = ""
+}
